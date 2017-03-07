9f8f476b43a806b31dbc569c4f8a2ea125a2421d
ryota kondo <aki_056@outlook.jp>
2017-03-07 23:20:41 +0900

    First Commit

---
+++ A-TL/A_TL/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace A_TL.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
