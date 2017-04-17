3717665bb5bbddf054259f2a4f471926223c0d12
ryota kondo <aki_056@outlook.jp>
2017-04-17 15:44:03 +0900

    画像を表示(Drawable型→Bitmap型→Byte[]型→ImageSorce型を経由)

--- ATL/ATL.Droid/GetApplicationIconAndName_Droid.cs
+++ ATL/ATL.Droid/GetApplicationIconAndName_Droid.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.Graphics;
 using Android.Graphics.Drawables;
 using Android.OS;
 using Android.Runtime;
@@ -31,10 +33,48 @@ namespace ATL.Droid
             NameAndUrl appNameAndIcon;
             appNameAndIcon.appName = appList.Where(b => b.PackageName == packageName).Select(a => a.LoadLabel(packageManager)).First();
 
-            var dr = packageManager.GetApplicationIcon(packageName);
+            var applicationIcon = packageManager.GetApplicationIcon(packageName);
 
-            appNameAndIcon.iconUrl = dr.ToString();
+            //----------------
+            var bitmap = drawableToBitmap(applicationIcon);
+
+            var memoryStream = new MemoryStream();
+            bitmap.Compress(Bitmap.CompressFormat.Png, 100, memoryStream);
+            appNameAndIcon.iconUrl = memoryStream.ToArray();
+            //----------
+
             return appNameAndIcon;
         }
+
+        public static Bitmap drawableToBitmap(Drawable drawable)
+        {
+            Bitmap bitmap = null;
+
+            if (drawable is BitmapDrawable) {
+                BitmapDrawable bitmapDrawable = (BitmapDrawable)drawable;
+                if (bitmapDrawable.Bitmap != null)
+                {
+                    return bitmapDrawable.Bitmap;
+                }
+            }
+
+            if (drawable.IntrinsicWidth <= 0 || drawable.IntrinsicHeight <= 0)
+            {
+                bitmap = Bitmap.CreateBitmap(1, 1, Bitmap.Config.Argb8888);
+            }
+            else
+            {
+                bitmap = Bitmap.CreateBitmap(drawable.IntrinsicWidth, drawable.IntrinsicHeight, Bitmap.Config.Argb8888);
+            }
+
+            Canvas canvas = new Canvas(bitmap);
+            drawable.SetBounds(0, 0, canvas.Width, canvas.Height);
+            drawable.Draw(canvas);
+            return bitmap;
+        }
+
+
     }
 }
+
+
--- ATL/ATL/Helpers/NameAndUrl.cs
+++ ATL/ATL/Helpers/NameAndUrl.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace ATL.Helpers
 {
     public struct NameAndUrl
     {
         public string appName;
-        public string iconUrl;
+        public byte[] iconUrl;
     }
 }
